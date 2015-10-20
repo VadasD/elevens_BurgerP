a064cdd32ed0211cea94f780a2605eb0ecdb504d
VadasD <burgerp@gilmour.org>
2015-10-20 15:03:05 -0400

    MORE WORK!

--- src/Deck.java
+++ src/Deck.java
@@ -1,64 +1,65 @@
+
 import java.util.List;
 import java.util.ArrayList;
 
 /**
- * The Deck class represents a shuffled deck of cards.
- * It provides several operations including
- *      initialize, shuffle, deal, and check if empty.
+ * The Deck class represents a shuffled deck of cards. It provides several
+ * operations including initialize, shuffle, deal, and check if empty.
  */
 public class Deck {
 
-	/**
-	 * cards contains all the cards in the deck.
-	 */
-	private List<Card> cards;
+    /**
+     * cards contains all the cards in the deck.
+     */
+    private List<Card> cards;
 
-	/**
-	 * size is the number of not-yet-dealt cards.
-	 * Cards are dealt from the top (highest index) down.
-	 * The next card to be dealt is at size - 1.
-	 */
-	private int size;
+    /**
+     * size is the number of not-yet-dealt cards. Cards are dealt from the top
+     * (highest index) down. The next card to be dealt is at size - 1.
+     */
+    private int size;
 
-
-	/**
-	 * Creates a new <code>Deck</code> instance.<BR>
-	 * It pairs each element of ranks with each element of suits,
-	 * and produces one of the corresponding card.
-	 * @param ranks is an array containing all of the card ranks.
-	 * @param suits is an array containing all of the card suits.
-	 * @param values is an array containing all of the card point values.
-	 */
-	public Deck(String[] ranks, String[] suits, int[] values) {
+    /**
+     * Creates a new <code>Deck</code> instance.<BR>
+     * It pairs each element of ranks with each element of suits, and produces
+     * one of the corresponding card.
+     *
+     * @param ranks is an array containing all of the card ranks.
+     * @param suits is an array containing all of the card suits.
+     * @param values is an array containing all of the card point values.
+     */
+    public Deck(String[] ranks, String[] suits, int[] values) {
         cards = new ArrayList<Card>();
         for (int j = 0; j < ranks.length; j++) {
-            for (String suitString : suits){
+            for (String suitString : suits) {
                 cards.add(new Card(ranks[j], suitString, values[j]));
             }
-	}
+        }
         size = cards.size();
+    }
+
+    /**
+     * Determines if this deck is empty (no undealt cards).
+     *
+     * @return true if this deck is empty, false otherwise.
+     */
+    public boolean isEmpty() {
+        if (size == 0) {
+            return true;
+        } else {
+            return false;
         }
 
+    }
+}
 
-	/**
-	 * Determines if this deck is empty (no undealt cards).
-	 * @return true if this deck is empty, false otherwise.
-	 */
-	public boolean isEmpty(){
-		if (size==0){
-                    return true;
-                }else{
-                    return false;
-                            }
-                            
-                }
-	}
-
-	/**
-	 * Accesses the number of undealt cards in this deck.
-	 * @return the number of undealt cards in this deck.
-	 */
-	public int size() {
+/**
+ * Accesses the number of undealt cards in this deck.
+ *
+ * @return the number of undealt cards in this deck.
+ */
+public int size() {
+        return size;
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
 	}
 
@@ -79,16 +80,19 @@ public class Deck {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
         // IS EMPTY if so return null
         size--;
+        if (size == 0);{
+            return null;
+        }else{
         Card c = cards.get(size);
         return c;
-	}
+    }
 
 	/**
 	 * Generates and returns a string representation of this deck.
 	 * @return a string representation of this deck.
 	 */
 	@Override
-	public String toString() {
+        public String toString() {
 		String rtn = "size = " + size + "\nUndealt cards: \n";
 
 		for (int k = size - 1; k >= 0; k--) {
